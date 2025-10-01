21eee412aa3d0c16ae21934f4f6ea91ebb0e025d
Jagdevsinh <jagdevsinh1247@gmail.com>
2025-10-01 12:28:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose=require("mongoose");
+const studentRoute=require("./Routes/studentRoute.js");
+const app = express();
+const PORT = 80;
+app.use(express.json());
+mongoose.connect(
+    "mongodb+srv:/",
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    }
+);
+app.use("/song",studentRoute);
+app.listen(PORT,()=>{
+    console.log("server is running :127.0.0.1"+PORT);
+});
